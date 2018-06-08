a157c188be1c0e63c8a819230a6a4d11a06c266d
ANTONIO <joelant97@gmail.com>
2018-06-08 22:25:09 +0000

    update main 1.1 + Error 404

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.notFound;
+
 
 public class Main {
 
@@ -16,27 +18,26 @@ public class Main {
         final Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
 
         cfg.setClassForTemplateLoading(Main.class, "/");
+        /* // Probando
+        get("/prueba", (req, res) -> "Spark + ..."); */
 
-        // Probando
-        //get("/prueba", (req, res) -> "Spark + ...");
         //ArrayList para Estudiantes.
         ArrayList<Estudiante> estudiantes = new ArrayList<>();
+
+        //listado de estudiantes
+        estudiantes.add(new Estudiante(20121776,"Joel","Rodriguez","809-555-5555"));
+        estudiantes.add(new Estudiante(20135555,"Frank","Perez","809-578-5555"));
+        estudiantes.add(new Estudiante(20150000,"Luis","Gomez","809-578-1212"));
 
         get("/", (request, response) -> {
 
             //Crear Modelo de Datos
             Map<String, Object> input = new HashMap<>();
             input.put("blogTitle", "Plantilla Freemarker Lista Estudiantes");
-
-
-            estudiantes.add(new Estudiante(20121776,"Joel","Rodriguez","809-555-5555"));
-            estudiantes.add(new Estudiante(20135555,"Frank","Perez","809-578-5555"));
-            estudiantes.add(new Estudiante(20150000,"Luis","Gomez","809-578-1212"));
-
             input.put("estudiantes", estudiantes);
 
             //Instanciar Plantilla
-            Template plantilla = cfg.getTemplate("plantillas/form.ftl");
+            Template plantilla = cfg.getTemplate("plantillas/principal.ftl");
 
             //Salida
             Writer consoleWriter = new StringWriter();
@@ -50,7 +51,8 @@ public class Main {
 
 
 
-
+        // Error 404
+        notFound("<html><body><h1>Error 404: Se ha comunicado con el Server, pero el recurso solicitado no existe</h1></body></html>");
 
 
     }
